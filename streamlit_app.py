2eb33f9cdf7ae481efc291d92d452e1cc69a70e7
Alysha Rashid <63248033+alysha-21@users.noreply.github.com>
2022-07-29 23:32:07 -0400

    Update streamlit_app.py
    update to make customer "thank you" cleaner

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.multiselect("What fruit would you like to add?", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[add_my_fruit]
-streamlit.write('Thank you for adding ', add_my_fruit)
+streamlit.write('Thank you for adding ', fruits_to_show)
