1a6764c59d2354fdf096860939208dd61ceb0aef
Alysha Rashid <63248033+alysha-21@users.noreply.github.com>
2022-07-29 23:29:46 -0400

    Update streamlit_app.py
    add second text entry box for customer to add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.multiselect("What fruit would you like to add?", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[add_my_fruit]
+streamlit.write('Thank you for adding ', add_my_fruit)
